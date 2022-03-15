aa6bf23cd33d8227d7df4463d4496f220777962a
Kacper <kacper0775@gmail.com>
2022-03-15 22:34:08 +0100

    update folder structure

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
-import App from "./src/components/App.js";
-import "./src/styles/base.scss";
+import App from "./src/components/app/App.js";
 
 const appRouting = (
   <Router>
