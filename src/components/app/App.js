a199b39ea1af7fad45ca0c1556ed10bb09ce8e3f
Kacper <kacper0775@gmail.com>
2022-03-17 19:36:20 +0100

    update folder structure

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,40 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Banner from "../banner/Banner";
-import Footer from "../footer/Footer";
-import Home from "../home/Home";
-import Nav from "../nav/Nav";
-import Standings from "../standings/Standings"
-import Results from "../results/Results";
-import Calendar from "../calendar/Calendar";
-import Driver from "../driver/Driver";
-import Map from "../map/Map";
-import Records from "../records/Records";
-import "./app.scss"
+import Banner from "../../layouts/banner/Banner";
+import Footer from "../../layouts/footer/Footer";
+import Main from "../../layouts/main/Main"
+import Nav from "../../layouts/nav/Nav"
+import "./App.scss"
 
 const App = () => {
-   
    return (
       <>
          <div id="page-wrapper">
-
             <Banner/>
             <Nav/>
-         
             <div id="content-wrapper">
-               <Routes>
-                  <Route exact path="/" element={<Home/>} />
-                  <Route path="/standings" element={<Standings/>} />
-                  <Route path="/results" element={<Results/>} />
-                  <Route path="/calendar" element={<Calendar/>} />
-                  <Route path="/driver" element={<Driver/>} />
-                  <Route path="/map" element={<Map/>} />
-                  <Route path="/records" element={<Records/>} />
-               </Routes>
+               <Main/>
             </div>
-            
             <Footer/>
-
          </div>
       </>
    )
