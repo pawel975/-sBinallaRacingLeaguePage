819e4c47d6c00f1aa2bee081e242dc7bdab679d4
Pawel  Kurek <pawel.kurek975@gmail.com>
2022-03-17 11:35:04 +0100

    added logo to banner

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,13 +1,15 @@
 import React from "react";
 import './banner.scss'
 import headerBannerImg from '../../assets/img/header.png';
+import sBinallaLogo from '../../assets/img/sBinallaLogo.svg';
 
 const Banner = () => {
 
     return (
         <>
             <div id="main-banner" role='banner'>
-                <img src={headerBannerImg}/>
+                <img id="bannerBackground" src={headerBannerImg}/>
+                <img id="logo" src={sBinallaLogo}/>
             </div>
         </>
     )
