7cee0f8d42ec77e3a30f987b5862d5ae6c34880e
Pawel  Kurek <pawel.kurek975@gmail.com>
2022-03-17 11:44:59 +0100

    update banner

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './banner.scss'
-import headerBannerImg from '../../assets/img/header.png';
+import headerBannerImg from '../../assets/img/header2.gif';
 import sBinallaLogo from '../../assets/img/sBinallaLogo.svg';
 
 const Banner = () => {
